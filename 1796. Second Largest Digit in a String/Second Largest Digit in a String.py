ffd2a47a229d94c9b24505a89cf34771c7cc47cd
ZakkyAi <zakkyalwafa@gmail.com>
2025-01-06 00:20:05 +0700

    psuh

--- 1796. Second Largest Digit in a String/Second Largest Digit in a String.py
+++ 1796. Second Largest Digit in a String/Second Largest Digit in a String.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def secondHighest(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        
+        numbers = [int(item) for item in s if item.isdigit()]
+        unique_numbers = list(set(numbers))
+        if len(unique_numbers) < 2:
+            return -1
+        return sorted(unique_numbers, reverse=True)[1]
