b5c7a3b7c84ed7d8deba42c5f31480798ecb98cd
ZakkyAi <zakkyalwafa@gmail.com>
2025-01-09 00:11:07 +0700

    push

--- Score of a String/Score of a String.py
+++ Score of a String/Score of a String.py
@@ -1,8 +1,7 @@
-class Solution:
-    def scoreOfString(self, s: str) -> int:
+class Solution(object):
+    def scoreOfString(self, s):
+
         score = 0
         for i in range(1, len(s)):
             score += abs(ord(s[i]) - ord(s[i - 1]))
         return score
-Solution = Solution()
-print(Solution.scoreOfString("aba"))
